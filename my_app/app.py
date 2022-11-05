098ce4e85a231728491f8a8abc8dbcb52017064b
mounir7586 <mounir7586@gmail.com>
2022-11-06 00:12:04 +0100

    Changement de PORT

--- my_app/app.py
+++ my_app/app.py
@@ -5,6 +5,10 @@ from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
 from flask import Flask, request, render_template
 import pickle
+import os from pml 
+import app 
+
+port = int(os.environ.get('PORT', 5000)) app.run(host='0.0.0.0', port=port)
 
 app = Flask(__name__)
 
